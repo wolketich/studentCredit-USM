b68af56870f060ee85dc917765895f5a80f45be0
Vladislav Cernega <walkcatxd@gmail.com>
2023-10-12 22:31:53 +0100

    Update app.py
    Code refactor

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 from flask import Flask, request, render_template, redirect, url_for
-import re, time, requests
+import re
+import time
+import requests
 from bs4 import BeautifulSoup
-
-
-allowed_IDNP = ['2002500133457', '2006048027786']
-error_message = "Codul personal este introdus gresit."
-global data
 
 app = Flask(__name__)
 
-def GetData(IDNP):
-    url = "http://studentcrd.usm.md/"
-    login_url = url  # The same URL is used for login
+# Constants
+ALLOWED_IDNP = ['2002500133457', '2006048027786']
+ERROR_MESSAGE = "Codul personal este introdus gresit."
+URL = "http://studentcrd.usm.md/"
 
+def fetch_student_data(IDNP):
+    """
+    Fetch student data from the given URL using the provided IDNP.
+    
+    Args:
+    - IDNP (str): The student's IDNP.
+    
+    Returns:
+    - str: The content of the student data if found, otherwise an error code.
+    """
     # Send a GET request to the website to get the initial form data
-    response = requests.get(url)
+    response = requests.get(URL)
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # Extract __VIEWSTATE and __EVENTVALIDATION from the hidden input fields
+    # Extract necessary data for POST request
     viewstate = soup.find('input', {'name': '__VIEWSTATE'})['value']
     eventvalidation = soup.find('input', {'name': '__EVENTVALIDATION'})['value']
 
-    # Prepare the form data for the POST request
     form_data = {
         '__VIEWSTATE': viewstate,
         '__EVENTVALIDATION': eventvalidation,
@@ -29,48 +36,43 @@ def GetData(IDNP):
         'btLogin': 'Login'
     }
 
-    # Send a POST request with the form data to simulate button click
-    response = requests.post(login_url, data=form_data)
+    # Send a POST request with the form data
+    response = requests.post(URL, data=form_data)
+    time.sleep(5)  # Wait for the response
 
-    # Wait for the next page to load (you may need to adjust the time if necessary)
-    time.sleep(5)
-
-    # Parse the content of the new page after the POST request
     soup = BeautifulSoup(response.text, 'html.parser')
-
-    # Check if the span with id "lbErr" and text "Codul personal nu corect" exists
     error_span = soup.find('span', {'id': 'lbErr', 'text': 'Codul personal nu corect'})
 
-    # If the error span exists, return 808
     if error_span:
         return 808
 
-    # Extract the content of the div with id "nav-tabContent"
     div_content = soup.find('div', {'id': 'nav-tabContent'})
-
-    # Return the content of the div as a string
     return str(div_content)
+
 @app.route('/')
 def index():
+    """Redirect to the home route."""
     return redirect(url_for('home'))
 
-@app.route('/Users/Student.aspx', methods=['GET', 'POST'])
+@app.route('Users/Students.aspx', methods=['GET', 'POST'])
 def home():
+    """Render the student data or the index page based on the provided IDNP."""
     if request.method == 'POST':
         IDNP = request.form.get('txtCodperson')
+        
         if re.match(r'^\d{13}$', IDNP):
-            if IDNP in allowed_IDNP:
-                student_path = "students/" + IDNP + ".html"
+            if IDNP in ALLOWED_IDNP:
+                student_path = f"students/{IDNP}.html"
                 with open(student_path, "r") as student_info:
                     data = student_info.read()
                     return render_template('base.html', tab_content=data)
             else:
-                data = GetData(IDNP)
-                if data == None:
-                    return render_template('index.html', error_message=error_message)
-                return render_template('base.html', tab_content=GetData(IDNP))
+                data = fetch_student_data(IDNP)
+                if data == 808:
+                    return render_template('index.html', error_message=ERROR_MESSAGE)
+                return render_template('base.html', tab_content=data)
         else:
-            return render_template('index.html', error_message=error_message)
+            return render_template('index.html', error_message=ERROR_MESSAGE)
     return render_template('index.html')
 
 if __name__ == '__main__':
